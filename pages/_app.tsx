00cd030a53eea0bc193a179a33d472a359bac821
cxii112 <lyakhovoy.d@ya.ru>
2022-01-12 19:42:30 +0500

    update deprecated dependencies

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
 import '../styles/globals.scss'
 import type {AppProps} from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({Component, pageProps}: AppProps) {
+    return <div>
+        <span role="button"
+              className="button is-primary"/>
+        <span role="button"
+              className="button is-danger"/>
+        <Component {...pageProps} />
+    </div>
 }
 
 export default MyApp
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,24 @@
 import type {NextPage} from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.scss'
+// import '../styles/Home.module.scss'
 
 const Home: NextPage = () => {
     return (
-        <div className={'container'}>
+        <div>
             <Head>
-                <title className={'container'}>Тренировка счета 2</title>
+                <title>Тренировка счета 2</title>
                 <meta name="description" content="Приложение для тренировки счета у детей начальной школы"/>
                 <link rel="icon" href="/favicon.ico"/>
             </Head>
 
-            <main className={styles.main}>
-
+            <main>
+                <span role="button"
+                      className="button is-primary"/>
+                <span role="button"
+                      className="button is-danger"/>
             </main>
 
-            <footer className={styles.footer}>
+            <footer>
 
             </footer>
         </div>
