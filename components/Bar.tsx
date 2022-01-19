591339e45e5015734cc9152b899bc986ad3aff52
cxii112 <lyakhovoy.d@ya.ru>
2022-01-19 19:42:36 +0500

    change params

--- components/Bar.tsx
+++ components/Bar.tsx
@@ -1,7 +1,7 @@
-import {ReactElement} from "react";
+import {ReactNode} from "react";
 
 export default function Bar(props: {
-    children?: ReactElement
+    children?: ReactNode
 }) {
     return (
         <div className={"bg-slate-100 px-4 py-2.5 flex justify-evenly rounded-lg w-full"}>
