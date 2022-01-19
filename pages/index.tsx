00b7768ba15b0a1bb13e595b739759024f7bdec0
cxii112 <lyakhovoy.d@ya.ru>
2022-01-19 19:41:46 +0500

    sum mock

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type {NextPage} from "next";
 import Head from "next/head";
 import Bar from "../components/Bar";
+import SumSettings from "../components/SumSettings";
 
 const Home: NextPage = () => {
     return (
@@ -10,23 +11,16 @@ const Home: NextPage = () => {
                 <meta name="description" content="Приложение для тренировки счета у детей начальной школы"/>
                 <link rel="icon" href="/favicon.ico"/>
             </Head>
-            <div className={"container relative max-w-3xl flex flex-col justify-between h-full"}>
+            <div className={"container relative max-w-xl flex flex-col justify-between h-full"}>
                 <main className={"bg-slate-100 h-fit rounded-lg p-4"}>
                     <div className={"flex justify-evenly text-2xl font-bold text-black"}>
                         Настройки
                     </div>
+                    <SumSettings/>
                 </main>
                 <Bar>
                     <button
-                        className={
-                            "text-sky-500" +
-                            " tracking-widest" +
-                            " border-2" +
-                            " border-sky-500" +
-                            " rounded-full" +
-                            " px-4 py-1 hover:border-green-500 " +
-                            "hover:text-green-500 " +
-                            "transition-all"}>
+                        className={"text-sky-500 tracking-widest border-2 border-sky-500 rounded-full px-4 py-1 hover:border-green-500 hover:text-green-500 transition-all"}>
                         Начать
                     </button>
                 </Bar>
